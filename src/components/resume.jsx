3a7605828c115ebf2dd8ca4ed6d0c8416c82561a
unknown <dammyvicky1245@gmail.com>
2023-12-03 21:36:22 +0000

    new updtaes

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -8,24 +8,81 @@ const Resume = () => {
                 Res<span>ume</span>
             </h2>
                    
-                   {/* Experience */}
             <div className={styles.educationrow}>
+                
+            <div className={styles.educationcolumn}>
+                    <h3 className={styles.title}>Skills</h3>
 
+                    <div className={styles.educationbox}>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <div className={styles.bluetext}>
+                                    Languages
+                                </div>
+                                <h3>
+                                  Javascript, Typescript
+                                </h3>
+                            </div>
+                        </div>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <div className={styles.bluetext}>
+                                    {/* react icon for calendar */}
+                                    Framework/Libraries
+                                </div>
+                                <h3>
+                                 React
+                                </h3>
+                            </div>
+                        </div>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <div className={styles.bluetext}>
+                                    Databases/Storage
+                                </div>
+                                <h3>
+                                    MongoDB
+                                </h3>
+                            </div>
+                        </div>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <div className={styles.bluetext}>
+                                    Tools
+                                </div>
+                                <h3>
+                                  Git
+                                </h3>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            
+
+                   {/* Experience */}
             <div className={styles.educationcolumn}>
                 <h3 className={styles.title}>Experience</h3>
 
                 <div className={styles.educationbox}>
                     <div className={styles.educationcontent}>
                         <div className={styles.content}>
-                           <div className={styles.year}>
-                                {/* react icon for calendar */}
-                                July 2023 - October 2023
-                           </div>
                             <h3>
                                 Doyenify - Front End Developer Intern
                             </h3>
+                           <div className={styles.bluetext}>
+                                {/* react icon for calendar */}
+                                July 2023 - October 2023
+                           </div>
                             {/* talk about work experience */}
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolore similique vero delectus rem ullam, explicabo, optio totam eligendi animi, magni vel.</p>
+                            <ul>
+                                <li>Developed a  Web Application from scratch  with Typescript and React</li>
+                            </ul>
+
                         </div>
                       </div>
                 </div>
@@ -42,33 +99,65 @@ const Resume = () => {
 
                         <div className={styles.educationcontent}>
                             <div className={styles.content}>
-                                <div className={styles.year}>
-                                    {/* react icon for calendar */}
-                                    2025
-                                </div>
                                 <h3>
                                   B.Sc Computer Science with Mathematics at Obafemi Awolowo University
                                 </h3>
+                                <div className={styles.bluetext}>
+                                    {/* react icon for calendar */}
+                                    2025
+                                </div>
                             </div>
                         </div>
 
                         <div className={styles.educationcontent}>
                             <div className={styles.content}>
-                                <div className={styles.year}>
-                                    {/* react icon for calendar */}
-                                    2017
-                                </div>
                                 <h3>
                                   Genius Royal Academy, Ibadan
                                 </h3>
+                                <div className={styles.bluetext}>
+                                    {/* react icon for calendar */}
+                                   July 2017
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                       
+              
 
-                
-            
+              <div className={styles.educationcolumn}>
+                    <h3 className={styles.title}>Other</h3>
+
+                    <div className={styles.educationbox}>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <h3>
+                                 Hobbies
+                                </h3>
+                                <div className={styles.bluetext}>
+                                    {/* react icon for calendar */}
+                                    Exploring,  Reading
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.educationcontent}>
+                            <div className={styles.content}>
+                                <h3>
+                                  Genius Royal Academy, Ibadan
+                                </h3>
+                                <div className={styles.bluetext}>
+                                    {/* react icon for calendar */}
+                                   July 2017
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                       
+            </div>
 
         </section>
     )
