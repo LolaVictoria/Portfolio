8fd909ff2ca265af7f3cd0af9df06a4b2bd6ed99
unknown <dammyvicky1245@gmail.com>
2023-12-04 21:09:21 +0000

    web page  responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,31 +1,79 @@
 import { Link } from "react-router-dom"
 import styles from "./navbar.module.css"
+import { useState } from "react"
 
 
 const Navbar = () => { 
+   const [nav, setNav] = useState(false)
 
     return (
         <div className={styles.navbar}>
             <p className={styles.logo}>Lola Victoria</p>
 
-            <nav className={styles.navlist}>
-                    <p className="active">
-                      <Link to="">Home</Link>
-                    </p>
+            <p
+              className={styles.menu}
+              onClick={
+                () => setNav(!nav)
+              }>
+              {nav ? "X" : "%"}
+            </p>
 
-                    <p>
-                      <Link to="">Project</Link>
-                    </p>
+            
+            {nav ?
+                 <nav className={styles.navlist_responsive}>
+                  <p
+              className={styles.menu_responsive}
+              onClick={
+                () => setNav(!nav)
+              }>
+              {nav ? "X" : "%"}
+            </p>
+              
+              <ul>
+
+                 <li className="active">
+                   <Link to="">Home</Link>
+                 </li>
+
+                 <li>
+                   <Link to="">Project</Link>
+                 </li>
 
 
-                    <p>
-                      <Link to="">Resume</Link>
-                    </p>
+                 <li>
+                   <Link to="">Resume</Link>
+                 </li>
 
-                    <p>
-                    <Link to="/contact">Contact</Link>
-                    </p>
+                 <li>
+                 <Link to="/contact">Contact</Link>
+                 </li>
+              </ul>
+          
              
+         </nav> : ""}
+           
+
+            <nav className={styles.navlist}>
+              <ul>
+
+                    <li className="active">
+                      <Link to="">Home</Link>
+                    </li>
+
+                    <li>
+                      <Link to="">Project</Link>
+                    </li>
+
+
+                    <li>
+                      <Link to="">Resume</Link>
+                    </li>
+
+                    <li>
+                    <Link to="/contact">Contact</Link>
+                    </li>
+             
+              </ul>
                 
             </nav>
         </div>
